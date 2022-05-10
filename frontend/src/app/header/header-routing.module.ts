e203073f813030f9a12a64a1594e6f5c46f349b3
Shipra Kumari <shipra.kumari@kcsitglobal.com>
2022-05-10 16:12:02 +0530

    First

---
+++ frontend/src/app/header/header-routing.module.ts
@@ -0,0 +1,27 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// // import {SignInComponent} from './sign-in/sign-in.component'
+// // import { SignUpComponent } from './sign-up/sign-up.component';
+// import {SigninComponent} from './signin/signin.component';
+// import {SignupComponent} from './signup/signup.component';
+// const routes: Routes = [
+//   {
+//     path:'',
+    
+    
+//     children: [{
+//       path: 'signin',
+//       component: SigninComponent
+//     },
+//     {
+//       path:'signup',
+//       component: SignupComponent
+//     }]
+//   }
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forChild(routes)],
+//   exports: [RouterModule]
+// })
+// export class HeaderRoutingModule { }
